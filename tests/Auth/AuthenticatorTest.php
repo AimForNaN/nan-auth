10ed8b397506afa12c82187faf01214f9c231a94
AimForNaN <aimfornan@localhost>
2026-07-21 15:00:14 -0400

    Update tests.

--- tests/Auth/AuthenticatorTest.php
+++ tests/Auth/AuthenticatorTest.php
@@ -4,11 +4,13 @@ use Illuminate\Database\Capsule\Manager as CapsuleManager;
 use NaN\Authentication\Codecs\SymmetricPasetoCodec;
 use NaN\Authentication\Credentials\Credential;
 use NaN\Authentication\CredentialType;
-use NaN\Authentication\Factors\PasswordFactor;
 use NaN\Authentication\Hashers\BcryptHasher;
 use NaN\Authentication\Identifiers\Identifier;
 use NaN\Authentication\IdentifierType;
 use NaN\Authentication\Middleware\Authenticator;
+use NaN\Authentication\Middleware\Factors\Password\PasswordValidator;
+use NaN\Authentication\Middleware\Providers\CredentialProvider;
+use NaN\Authentication\Middleware\Providers\IdentifierProvider;
 use NaN\Authentication\Schemas\CredentialSchema;
 use NaN\Authentication\Schemas\IdentifierSchema;
 use NaN\Authentication\Stores\Sql\SqlCredentialStore;
@@ -21,36 +23,47 @@ use NaN\Http\RequestValidators\PostRequestValidator;
 use NaN\Http\ResponseFactory;
 use NaN\Http\ServerRequestFactory;
 use Nette\Schema\Expect;
-use Psr\Http\Message\ServerRequestInterface;
 
 describe('Authenticator', function () {
 	test('Password middleware', function () {
 		$pdo = CapsuleManager::connection()->getPdo();
 		$connection = new SqlConnection($pdo);
 		$shared_key = \random_bytes(32);
-		$auth = new Authenticator(
-			new PasswordFactor(
+		$middleware = new MiddlewareCollection(
+			new IdentifierProvider(
 				new PostRequestValidator(Expect::array([
 					'identifier' => new IdentifierSchema(Identifier::class, IdentifierType::Email),
+				])),
+				new SqlIdentifierStore($connection),
+			),
+			new CredentialProvider(
+				new PostRequestValidator(Expect::array([
 					'credential' => new CredentialSchema(Credential::class, CredentialType::Password),
 				])),
+			),
+			new PasswordValidator(
 				new SqlIdentityStore($connection),
-				new SqlIdentifierStore($connection),
 				new SqlCredentialStore($connection),
 				new BcryptHasher(),
 			),
-			new SqlSessionStore($connection),
-			new SymmetricPasetoCodec($shared_key)
+			new Authenticator(
+				new SqlSessionStore($connection),
+				new SymmetricPasetoCodec($shared_key)
+			),
 		);
-		$request = new ServerRequestFactory()->createServerRequest('POST', '/')
+		$request = new ServerRequestFactory()
+			->createServerRequest('POST', '/')
 			->withParsedBody([
 				'identifier' => 'test@example.com',
 				'credential' => 'password',
 			])
 		;
-		$rsp = $auth->process($request, new ClosureRequestHandler(function (ServerRequestInterface $req) {
-			return new ResponseFactory()->createResponse(200);
-		}));
+		$rsp = $middleware->process(
+			$request,
+			new ClosureRequestHandler(function () {
+				return new ResponseFactory()->createResponse(200);
+			}),
+		);
 
 		expect($rsp->getStatusCode())->toBe(200);
 	});
