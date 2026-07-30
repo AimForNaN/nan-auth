0cb7b89bdc85560753139efdb896769df247a9c1
AimForNaN <aimfornan@localhost>
2026-07-30 11:52:29 -0400

    Update tests.

--- tests/Auth/AuthenticatorTest.php
+++ tests/Auth/AuthenticatorTest.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Capsule\Manager as CapsuleManager;
+use NaN\Authentication\Caches\RedisCache;
 use NaN\Authentication\Codecs\SymmetricPasetoCodec;
 use NaN\Authentication\Credentials\Credential;
 use NaN\Authentication\CredentialType;
@@ -8,6 +9,7 @@ use NaN\Authentication\Hashers\BcryptHasher;
 use NaN\Authentication\Identifiers\Identifier;
 use NaN\Authentication\IdentifierType;
 use NaN\Authentication\Middleware\Authenticator;
+use NaN\Authentication\Middleware\Factors\Otp\OtpValidator;
 use NaN\Authentication\Middleware\Factors\Password\PasswordValidator;
 use NaN\Authentication\Middleware\Providers\CredentialProvider;
 use NaN\Authentication\Middleware\Providers\IdentifierProvider;
@@ -25,7 +27,48 @@ use NaN\Http\ServerRequestFactory;
 use Nette\Schema\Expect;
 
 describe('Authenticator', function () {
-	test('Password middleware', function () {
+	test('OTPs', function () {
+		$pdo = CapsuleManager::connection()->getPdo();
+		$connection = new SqlConnection($pdo);
+		$shared_key = \random_bytes(32);
+		$middleware = new MiddlewareCollection(
+			new IdentifierProvider(
+				new PostRequestValidator(Expect::array([
+					'identifier' => new IdentifierSchema(Identifier::class, IdentifierType::Email),
+				])),
+				new SqlIdentifierStore($connection),
+			),
+			new CredentialProvider(
+				new PostRequestValidator(Expect::array([
+					'credential' => new CredentialSchema(Credential::class, CredentialType::Password),
+				])),
+			),
+			new OtpValidator(
+				new RedisCache(new \Predis\Client()),
+			),
+			new Authenticator(
+				new SqlSessionStore($connection),
+				new SymmetricPasetoCodec($shared_key)
+			),
+		);
+		$request = new ServerRequestFactory()
+			->createServerRequest('POST', '/')
+			->withParsedBody([
+				'identifier' => 'test@example.com',
+				'credential' => '12345678',
+			])
+		;
+		$rsp = $middleware->process(
+			$request,
+			new ClosureRequestHandler(function () {
+				return new ResponseFactory()->createResponse(200);
+			}),
+		);
+
+		expect($rsp->getStatusCode())->toBe(200);
+	});
+
+	test('Passwords', function () {
 		$pdo = CapsuleManager::connection()->getPdo();
 		$connection = new SqlConnection($pdo);
 		$shared_key = \random_bytes(32);
@@ -43,7 +86,6 @@ describe('Authenticator', function () {
 			),
 			new PasswordValidator(
 				new SqlIdentityStore($connection),
-				new SqlCredentialStore($connection),
 				new BcryptHasher(),
 			),
 			new Authenticator(
